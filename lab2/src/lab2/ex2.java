ce43f77618e66e9ee0454606cd074b4d8cca004c
alexbrinzea <alexbrinzea19@gmail.com>
2025-01-17 00:12:10 +0200

    lab2

---
+++ lab2/src/lab2/ex2.java
@@ -0,0 +1,29 @@
+package lab2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintStream printStream = new PrintStream(new File("out.txt"));
+        String s = " ";
+        Vers vers = new Vers();
+        Scanner scanner = new Scanner(new File("cantec_in.txt"));
+        while(scanner.hasNextLine())
+        {
+            s = scanner.nextLine();
+            vers.randomGen();
+            if(vers.randomGen() <= 0.1F) {
+                printStream.println(s.toUpperCase() + vers.VerificareFinal(s) + " (Numar cuv: " + vers.nrCuvinte(s)
+                        + "). " + " (Vocale: " + vers.Vowels() + ").");
+            }
+            else
+            {
+                printStream.println(s + vers.VerificareFinal(s) + " (Numar cuv: " + vers.nrCuvinte(s) + "). " +
+                        " (Vocale: " + vers.Vowels() + ").");
+            }
+        }
+    }
+}
